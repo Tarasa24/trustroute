56ff1fee558fcddcff46c39044a535acf835e6cc
Tarasa24 <petr.gajdosik2424@gmail.com>
2023-08-12 01:34:41 +0200

    Add long and short key id methods

--- app/models/key.rb
+++ app/models/key.rb
@@ -12,4 +12,12 @@ class Key
 
   has_many :out, :vouches_for, rel_class: :VouchRelationship
   has_many :in, :is_vouched_for_by, rel_class: :VouchRelationship
+
+  def long_key_id
+    format('%08X', key_id).upcase.scan(/.{1,4}/).join(' ')
+  end
+
+  def short_key_id
+    long_key_id.split(' ')[-2..].join(' ')
+  end
 end
