f0becdbc4e9cdab9471cb0e5748aa79b31b7dd4f
Tarasa24 <petr.gajdosik2424@gmail.com>
2024-01-10 02:44:03 +0100

    Update initializer

--- config/initializers/pgpme.rb
+++ config/initializers/pgpme.rb
@@ -1,1 +1,20 @@
-GPGME::Engine.home_dir = ENV.fetch("GNUPGHOME", "./.gnupg")
+home_dir = ENV.fetch("GNUPGHOME", Rails.root.join(".gnupg").to_s)
+GPGME::Engine.home_dir = home_dir
+
+# Ensure that the home directory exists
+FileUtils.mkdir_p(home_dir)
+
+# if common.conf does not exist, initialize gpg directory
+common_conf = File.join(home_dir, "common.conf")
+unless File.exist?(common_conf)
+  gpg_bin = GPGME::Engine.info.first.file_name
+  system("#{gpg_bin} --homedir #{home_dir} --list-keys")
+end
+
+# if config.gpg does not exist, create it and add keyserver
+config_gpg = File.join(home_dir, "gpg.conf")
+unless File.exist?(config_gpg)
+  File.open(config_gpg, "w") do |f|
+    f.puts("keyserver #{ENV.fetch("KEYSERVER", "hkp://keys.openpgp.org")}")
+  end
+end
