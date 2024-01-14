d79cb9a3d90e22eb48ca8d9ef66e5bfe38d5334d
Tarasa24 <petr.gajdosik2424@gmail.com>
2024-01-14 01:56:11 +0100

    Simplify initializer

--- config/initializers/pgpme.rb
+++ config/initializers/pgpme.rb
@@ -10,11 +10,3 @@ unless File.exist?(common_conf)
   gpg_bin = GPGME::Engine.info.first.file_name
   system("#{gpg_bin} --homedir #{home_dir} --list-keys")
 end
-
-# if config.gpg does not exist, create it and add keyserver
-config_gpg = File.join(home_dir, "gpg.conf")
-unless File.exist?(config_gpg)
-  File.open(config_gpg, "w") do |f|
-    f.puts("keyserver #{ENV.fetch("KEYSERVER", "hkp://keys.openpgp.org")}")
-  end
-end
