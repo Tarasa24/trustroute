647dbedbe900b7b737c5c048fa285e527766d254
Tarasa24 <petr.gajdosik2424@gmail.com>
2024-01-17 13:30:03 +0100

    Implement tests for keys controller
    TODO: Make rspec use testing database

---
+++ spec/rails_helper.rb
@@ -0,0 +1,40 @@
+require "spec_helper"
+# ENV["RAILS_ENV"] ||= "test" TODO: Fix this to use testing database
+require_relative "../config/environment"
+
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require "rspec/rails"
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  abort e.to_s.strip
+end
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+
+  # Before starting the test suite, create new gpg keyring
+  config.before(:suite) do
+    FileUtils.mkdir_p("tmp/gpg")
+    GPGME::Engine.home_dir = "tmp/gpg"
+  end
+
+  # Before each test, clear the database
+  config.before(:each) do
+    ActiveGraph::Base.query("MATCH (n) DETACH DELETE n")
+  end
+
+  # After each test, clear the keyring
+  config.after(:each) do
+    GPGME::Key.find(:public).each(&:delete!)
+    GPGME::Key.find(:secret).each(&:delete!)
+  end
+
+  # After the test suite, delete the keyring
+  config.after(:suite) do
+    FileUtils.rm_rf("tmp/gpg")
+  end
+end
