8236e679f209723a217e8e54193101aa6b6f8715
Tarasa24 <petr.gajdosik2424@gmail.com>
2024-02-09 23:27:31 +0100

    Use simplified force graph

--- app/frontend/controls/javascripts/graph_canvas.js
+++ app/frontend/controls/javascripts/graph_canvas.js
@@ -1,5 +1,91 @@
 import * as d3 from 'd3';
 
-export default function graphCanvas() {
-  return document.createElement('canvas');
+const WIDTH = 928;
+const HEIGHT = 600;
+
+export default function graphCanvas(data) {
+  const nodes = data.nodes;
+  const links = data.links;
+
+  let svg = d3.create('svg')
+    .attr('width', WIDTH)
+    .attr('height', HEIGHT)
+    .style('background-color', 'lightgrey');
+
+  const simulation = d3.forceSimulation(nodes)
+    .force("link", d3.forceLink(links).id(d => d.id))
+    .force("charge", d3.forceManyBody().strength(-400))
+    .force("x", d3.forceX())
+    .force("y", d3.forceY());
+
+  const link = svg.append("g")
+    .attr("fill", "none")
+    .attr("stroke-width", 1.5)
+    .selectAll("path")
+    .data(links)
+    .join("path")
+    .attr("stroke", d => color(d.type))
+    .attr("marker-end", d => `url(${new URL(`#arrow-${d.type}`, location)})`);
+
+  const node = svg.append("g")
+    .attr("fill", "currentColor")
+    .attr("stroke-linecap", "round")
+    .attr("stroke-linejoin", "round")
+    .selectAll("g")
+    .data(nodes)
+    .join("g")
+    .call(drag(simulation));
+
+  node.append("circle")
+    .attr("stroke", "white")
+    .attr("stroke-width", 1.5)
+    .attr("r", 4);
+
+  node.append("text")
+    .attr("x", 8)
+    .attr("y", "0.31em")
+    .text(d => d.id)
+    .clone(true).lower()
+    .attr("fill", "none")
+    .attr("stroke", "white")
+    .attr("stroke-width", 3);
+
+  simulation.on("tick", () => {
+    link.attr("d", linkArc);
+    node.attr("transform", d => `translate(${d.x},${d.y})`);
+  });
+
+  return svg.node();
+}
+
+const drag = simulation => {
+  function dragstarted(event, d) {
+    if (!event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+  }
+
+  function dragged(event, d) {
+    d.fx = event.x;
+    d.fy = event.y;
+  }
+
+  function dragended(event, d) {
+    if (!event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+  }
+
+  return d3.drag()
+    .on("start", dragstarted)
+    .on("drag", dragged)
+    .on("end", dragended);
+};
+
+function linkArc(d) {
+  const r = Math.hypot(d.target.x - d.source.x, d.target.y - d.source.y);
+  return `
+    M${d.source.x},${d.source.y}
+    A${r},${r} 0 0,1 ${d.target.x},${d.target.y}
+  `;
 }
