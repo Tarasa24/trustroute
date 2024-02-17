f0d297d3ce055aee23d142a66c15544beedeb8e2
Tarasa24 <petr.gajdosik2424@gmail.com>
2024-02-17 23:34:53 +0100

    Make vouches work

--- app/models/vouch_relationship.rb
+++ app/models/vouch_relationship.rb
@@ -12,12 +12,4 @@ class VouchRelationship
   property :signature_id, type: Integer
 
   validates :signature_id, presence: true
-
-  def self.between_master_keys
-    query = "MATCH (from:Key)-[rel:vouches_for]->(to:Key) "
-    query += "WHERE from.master = true AND to.master = true "
-    query += "RETURN from, rel, to"
-
-    ActiveGraph::Base.query(query)
-  end
 end
