9d0b8103ac6c9c4adff3b0cde465a8089c19432b
Tarasa24 <petr.gajdosik2424@gmail.com>
2024-02-21 20:52:21 +0100

    Add ruby-lsp-rails

--- shell.nix
+++ shell.nix
@@ -4,11 +4,11 @@
     nativeBuildInputs = with pkgs.buildPackages; [
       ruby_3_3
       rubyPackages_3_3.gpgme rubyPackages_3_3.ffi rubyPackages_3_3.nokogiri rubyPackages_3_3.sassc
-      rubyPackages_3_3.ruby-lsp
+      rubyPackages_3_3.ruby-lsp rubyPackages_3_3.prism
       nodejs yarn
       overmind
       redis
-      libstdcxx5 zlib xz libxml2
+      libstdcxx5 zlib xz libxml2 prism
     ];
 
     shellHook = ''
