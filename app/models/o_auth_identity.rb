64a250b2337980e07b5338d5c6051d2859f6b4d8
Tarasa24 <petr.gajdosik2424@gmail.com>
2024-02-27 18:44:48 +0100

    Add encrypted token to OauthIdentity

--- app/models/o_auth_identity.rb
+++ app/models/o_auth_identity.rb
@@ -7,7 +7,44 @@ class OAuthIdentity
   property :validated, type: Boolean, default: false
   property :uid, type: String
   enum provider: PROVIDERS
+  property :encrypted_token, type: String
 
   validates :uid, presence: true, uniqueness: {scope: :provider}
   validates :provider, presence: true
+  validate :validate_token, if: -> { encrypted_token.present? }
+  validates :encrypted_token, presence: true, if: -> { validated? }
+
+  def token
+    return @token if defined?(@token)
+
+    return nil if encrypted_token.blank?
+
+    @token = begin
+      cipher = OpenSSL::Cipher.new("aes-256-cbc")
+      cipher.decrypt
+      cipher.key = Rails.application.credentials.secret_key_base[0..31]
+      cipher.iv = Rails.application.credentials.secret_key_base[0..15]
+      cipher.update(Base64.strict_decode64(encrypted_token)) + cipher.final
+    end
+  end
+
+  def token=(value)
+    cipher = OpenSSL::Cipher.new("aes-256-cbc")
+    cipher.encrypt
+    cipher.key = Rails.application.credentials.secret_key_base[0..31]
+    cipher.iv = Rails.application.credentials.secret_key_base[0..15]
+    self.encrypted_token = Base64.strict_encode64(cipher.update(value) + cipher.final)
+    @token = value
+  end
+
+  def encrypted_token=(value)
+    @token = nil
+    super
+  end
+
+  def validate_token
+    Base64.strict_decode64(encrypted_token)
+  rescue ArgumentError
+    errors.add(:encrypted_token, "is not in base64 format")
+  end
 end
