5a971af64e29346ea108235019e94f24349b3327
Tarasa24 <petr.gajdosik2424@gmail.com>
2024-02-29 01:03:14 +0100

    Export property encryption to concern

--- app/models/o_auth_identity.rb
+++ app/models/o_auth_identity.rb
@@ -2,52 +2,24 @@ class OAuthIdentity
   include ActiveGraph::Node
   include ActiveGraph::Timestamps
 
-  PROVIDERS = %i[github]
+  include PropertyEncryptable
 
   property :validated, type: Boolean, default: false
   property :uid, type: String
-  enum provider: PROVIDERS
+  enum provider: OmniAuth::Builder.providers
   property :encrypted_token, type: String
+  property :encrypted_secret, type: String
   property :identity_data, type: Hash
+
+  property_encryptable :encrypted_token, :encrypted_secret
 
   validates :uid, presence: true, uniqueness: {scope: :provider}, if: -> { validated? }
   validates :provider, presence: true
-  validate :validate_token, if: -> { encrypted_token.present? }
   validates :encrypted_token, presence: true, if: -> { validated? }
 
   has_one :in, :key, type: :has_identity, model_class: :Key
 
-  def token
-    return @token if defined?(@token)
-
-    return nil if encrypted_token.blank?
-
-    @token = begin
-      cipher = OpenSSL::Cipher.new("aes-256-cbc")
-      cipher.decrypt
-      cipher.key = Rails.application.credentials.secret_key_base[0..31]
-      cipher.iv = Rails.application.credentials.secret_key_base[0..15]
-      cipher.update(Base64.strict_decode64(encrypted_token)) + cipher.final
-    end
-  end
-
-  def token=(value)
-    cipher = OpenSSL::Cipher.new("aes-256-cbc")
-    cipher.encrypt
-    cipher.key = Rails.application.credentials.secret_key_base[0..31]
-    cipher.iv = Rails.application.credentials.secret_key_base[0..15]
-    self.encrypted_token = Base64.strict_encode64(cipher.update(value) + cipher.final)
-    @token = value
-  end
-
-  def encrypted_token=(value)
-    @token = nil
-    super
-  end
-
-  def validate_token
-    Base64.strict_decode64(encrypted_token)
-  rescue ArgumentError
-    errors.add(:encrypted_token, "is not in base64 format")
+  def credentials_expire?
+    !(encrypted_token.present? && encrypted_secret.blank?)
   end
 end
