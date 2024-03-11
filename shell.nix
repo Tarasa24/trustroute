6b3593403a0bd7d48e34bc61e8f0a31ef712b10c
Tarasa24 <petr.gajdosik2424@gmail.com>
2024-03-11 23:40:50 +0100

    Fix bundle failed install

--- shell.nix
+++ shell.nix
@@ -3,7 +3,7 @@
     # nativeBuildInputs is usually what you want -- tools you need to run
     nativeBuildInputs = with pkgs.buildPackages; [
       ruby_3_3
-      rubyPackages_3_3.gpgme rubyPackages_3_3.ffi rubyPackages_3_3.nokogiri rubyPackages_3_3.sassc
+      rubyPackages_3_3.ffi rubyPackages_3_3.nokogiri rubyPackages_3_3.sassc
       rubyPackages_3_3.ruby-lsp rubyPackages_3_3.prism
       nodejs yarn
       overmind
