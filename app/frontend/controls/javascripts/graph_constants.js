2f9f94352507683122bda4ac67a1b2910e91e24e
Tarasa24 <petr.gajdosik2424@gmail.com>
2024-03-17 18:52:15 +0100

    Move graph color definitions into scss

--- app/frontend/controls/javascripts/graph_constants.js
+++ app/frontend/controls/javascripts/graph_constants.js
@@ -1,9 +1,27 @@
+import colors from '../../stylesheets/variables.scss?raw';
+
+const colorsObj = {};
+for (let line of colors.split("\n")) {
+  if (!line.match(/\$.*: (#.*)|(\$.*);/)) continue;
+
+  let [key, value] = line.split(':');
+  key = key.trim();
+  value = value.replace(';', '').trim();
+  colorsObj[key] = value;
+
+  if (value.includes('$')) {
+    if (!colorsObj[value]) continue;
+    colorsObj[key] = colorsObj[value];
+  }
+}
+
 export const WIDTH = 928;
 export const HEIGHT = 600;
-export const LINK_COLOR = '#3F3F3F';
-export const ARROW_COLOR = LINK_COLOR;
-export const NODE_COLOR = '#BEC6CF';
-export const SELF_NODE_COLOR = '#28A4D9';
-export const TARGET_NODE_COLOR = '#FF7A00';
-export const BACKGROUND_COLOR = '#1C2127';
+
+export const LINK_COLOR = colorsObj['$LINK_COLOR'];
+export const ARROW_COLOR = colorsObj['$ARROW_COLOR'];
+export const NODE_COLOR = colorsObj['$NODE_COLOR'];
+export const SELF_NODE_COLOR = colorsObj['$SELF_NODE_COLOR'];
+export const TARGET_NODE_COLOR = colorsObj['$TARGET_NODE_COLOR'];
+export const BACKGROUND_COLOR = colorsObj['$BACKGROUND_COLOR'];
 export const TEXT_COLOR = 'white';
