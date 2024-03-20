8d4ebd26a0bd4460ae66aaa80f3eedb34efae28b
Tarasa24 <petr.gajdosik2424@gmail.com>
2024-03-20 02:43:48 +0100

    Use svg snapshot to show before fetching is done

--- app/frontend/controls/javascripts/graph.js
+++ app/frontend/controls/javascripts/graph.js
@@ -11,8 +11,9 @@ async function renderGraph() {
   container.innerHTML = '';
   container.appendChild(graphCanvas(data));
 
-  // Clear the saved graph state from local storage
+  // Clear the saved graph state and snapshot from local storage
   localStorage.removeItem('graph_data');
+  localStorage.removeItem('graph_svg');
 }
 
 function saveGraph() {
@@ -20,18 +21,27 @@ function saveGraph() {
   if (container === null) return;
 
   saveLocalGraphData(container);
+  localStorage.setItem('graph_svg', container.innerHTML);
 }
 
 async function reRenderGraph() {
   const container = document.getElementById('chart');
   if (container === null) return;
 
-  // Use the saved graph state from local storage
+  // Initially display svg snapshot from local storage
+  const svg = localStorage.getItem('graph_svg');
+  if (svg !== null) container.innerHTML = svg;
+
+  // Start fetching and preparing the new state
   const data = await fetchLocalGraphData();
   if (data === null) return renderGraph(); // We should re-fetch
 
+  // Finally clear the container and render the new graph
   container.innerHTML = '';
   container.appendChild(graphCanvas(data));
+
+  // Clear the snapshot from local storage
+  localStorage.removeItem('graph_svg');
 }
 
 document.addEventListener('DOMContentLoaded', () => renderGraph());
