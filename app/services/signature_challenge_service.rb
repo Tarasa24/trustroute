8790571845d076517bbe5e6b98dcea4a96a5cf1a
Tarasa24 <petr.gajdosik2424@gmail.com>
2024-03-23 12:41:07 +0100

    Use ApplicationService

--- app/services/signature_challenge_service.rb
+++ app/services/signature_challenge_service.rb
@@ -1,23 +1,21 @@
-class SignatureChallengeService
-  attr_reader :key, :nonce, :signature
-
-  def initialize(key, nonce, signature)
-    @key = key # Key object
-    @nonce = nonce # String
-    @signature = signature # GPGME::Data
-  end
+class SignatureChallengeService < ApplicationService
+  param :key # Key
+  param :nonce # String
+  param :signature # GPGME::Data
 
   # Verifies that that key public key signed the nonce
   # Returns true if the signature is valid, false otherwise
   def call
     full_signature_check || detached_signature_check
+  rescue GPGME::Error => e
+    error(:gpgme_error, e.message)
   end
 
   def detached_signature_check
     crypto = GPGME::Crypto.new
     crypto.verify(signature, signed_text: nonce + "\n") do |signature|
-      return false unless signature.valid?
-      return false unless signature.fpr.upcase == key.fingerprint.to_s(16).upcase
+      return error(:invalid_signature, "") unless signature.valid?
+      return error(:invalid_signer, "") unless signature.fpr.upcase == key.fingerprint.to_s(16).upcase
     end
 
     true
@@ -26,10 +24,10 @@ class SignatureChallengeService
   def full_signature_check
     crypto = GPGME::Crypto.new
     signed_text = crypto.verify(signature) do |signature|
-      return false unless signature.valid?
-      return false unless signature.fpr.upcase == key.fingerprint.to_s(16).upcase
+      return error(:invalid_signature, "") unless signature.valid?
+      return error(:invalid_signer, "") unless signature.fpr.upcase == key.fingerprint.to_s(16).upcase
     end
-    return false unless signed_text&.include?(nonce)
+    return error(:incorrect_nonce, "") unless signed_text&.include?(nonce)
 
     true
   end
