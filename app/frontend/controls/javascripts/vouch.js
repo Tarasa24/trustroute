0d06647474a3cbd594ed85d02172aab31e60704a
Tarasa24 <petr.gajdosik2424@gmail.com>
2024-03-29 00:05:43 +0100

    Register autosubmit and async redirect channel

--- app/frontend/controls/javascripts/vouch.js
+++ app/frontend/controls/javascripts/vouch.js
@@ -1,10 +1,21 @@
-import { registerFormatOptionListener } from "./shared";
+import { registerFormatOptionListener, registerAutoSubmitForm } from './shared';
+import * as channel from "./channels/async_redirect";
 
 function registerListeners() {
   const container = document.querySelector('.vouch');
   if (!container) return;
 
   registerFormatOptionListener();
+  registerAutoSubmitForm();
+  connect();
+}
+
+function connect() {
+  const container = document.querySelector('.vouch');
+  if (!container) return;
+
+  const keyId = container.dataset.keyId;
+  if (keyId) channel.connect(`vouch:${document.current_key}:${keyId}`);
 }
 
 document.addEventListener('DOMContentLoaded', registerListeners);
